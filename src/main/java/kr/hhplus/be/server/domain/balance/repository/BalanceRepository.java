874514b9e74f4531aba2f327beff13a87fef5907
Sejeong Lee <sejeong0516@gmail.com>
2025-01-06 19:35:37 +0900

    Refactor: lock Impl -> Repository

--- src/main/java/kr/hhplus/be/server/domain/balance/repository/BalanceRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/balance/repository/BalanceRepository.java
@@ -1,12 +1,21 @@
 package kr.hhplus.be.server.domain.balance.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import kr.hhplus.be.server.domain.balance.dto.ChargeCommand;
 import kr.hhplus.be.server.domain.balance.entity.Balance;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 public interface BalanceRepository {
     Balance getBalance(Long userId);
 
     Balance chargeBalance(ChargeCommand command);
 
-    Balance getBalanceWithLock(Long userId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
+    @Query("select b from Balance b where b.userId = :userId")
+    Balance getBalanceWithLock(@Param("userId") Long userId);
 }
--- src/main/java/kr/hhplus/be/server/infra/balance/BalanceRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infra/balance/BalanceRepositoryImpl.java
@@ -1,17 +1,14 @@
 package kr.hhplus.be.server.infra.balance;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.balance.dto.ChargeCommand;
 import kr.hhplus.be.server.domain.balance.entity.Balance;
 import kr.hhplus.be.server.domain.balance.repository.BalanceRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 @Repository
 public interface BalanceRepositoryImpl extends JpaRepository<Balance, Long>, BalanceRepository {
@@ -31,16 +28,5 @@ public interface BalanceRepositoryImpl extends JpaRepository<Balance, Long>, Bal
                 .build();
         return save(balance);
     }
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select b from Balance b where b.userId = :userId")
-    Optional<Balance> findByUserIdWithLock(@Param("userId") Long userId);
-
-    @Override
-    default Balance getBalanceWithLock(Long userId) {
-        return findByUserIdWithLock(userId)
-                .orElseThrow(() -> new EntityNotFoundException("유저 ID에 해당하는 잔고가 없습니다. " + userId));
-    }
-
 
 }
