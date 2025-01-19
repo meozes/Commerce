9fc83fa0f83b4ef0ca33b6b600b799392ab6020f
Sejeong Lee <sejeong0516@gmail.com>
2025-01-19 22:25:42 +0900

    Test: 여러 유저가 동시에 쿠폰 발급 시 성능 테스트

--- src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponRepository.java
@@ -16,12 +16,12 @@ public interface CouponRepository {
     Coupon save(Coupon coupon);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
     @Query("select c from Coupon c where c.id = :couponId")
     Optional<Coupon> getCouponWithLock(@Param("couponId") Long couponId);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
     @Query("select c from Coupon c join IssuedCoupon ic on c = ic.coupon where ic.orderId = :orderId and ic.userId = :userId")
     Optional<Coupon> getCouponWithLock(@Param("orderId") Long orderId, @Param("userId") Long userId);
 }
