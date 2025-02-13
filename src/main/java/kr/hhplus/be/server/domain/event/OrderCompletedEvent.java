19621696ac2af03651d3963c7a3e9cb43643fbe4
Sejeong Lee <sejeong0516@gmail.com>
2025-02-13 20:29:44 +0900

    Feat: 주문 완료 정보 전송 event 구조로 변경

---
+++ src/main/java/kr/hhplus/be/server/domain/event/OrderCompletedEvent.java
@@ -0,0 +1,26 @@
+package kr.hhplus.be.server.domain.event;
+
+import kr.hhplus.be.server.domain.order.entity.Order;
+import kr.hhplus.be.server.domain.order.type.OrderStatusType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class OrderCompletedEvent {
+    private final Long orderId;
+    private final Long userId;
+    private final OrderStatusType orderStatus;
+    private final LocalDateTime completedAt;
+
+    public static OrderCompletedEvent from(Order order) {
+        return OrderCompletedEvent.builder()
+                .orderId(order.getId())
+                .userId(order.getUserId())
+                .orderStatus(order.getOrderStatus())
+                .completedAt(LocalDateTime.now())
+                .build();
+    }
+}
